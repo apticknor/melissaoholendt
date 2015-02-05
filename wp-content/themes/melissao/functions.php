69f33c05ab2682bfa6418e7ef5e8d9e98e37f15b
Anthony Ticknor <tony.ticknor@gmail.com>
2015-02-05 12:44:47 -0600

    adding in skeleton of custom theme

---
+++ wp-content/themes/melissao/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+/* ====================================================================================================
+ Theme Configuration
+==================================================================================================== */
+add_theme_support('post-thumbnails');
+add_theme_support('automatic-feed-links');
+add_theme_support('menus');
+
+remove_theme_support('post-formats');
+remove_theme_support('custom-background');
+remove_theme_support('custom-header');
