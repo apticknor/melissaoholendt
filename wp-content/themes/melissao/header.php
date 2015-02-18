f770460925f7764ec89d9a4d7a0c9b88d3be9d6b
Anthony Ticknor <tony.ticknor@gmail.com>
2015-02-17 21:44:34 -0600

    markup to accompany previous masthead refactor

--- wp-content/themes/melissao/header.php
+++ wp-content/themes/melissao/header.php
@@ -8,7 +8,7 @@
 ?>
 
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> class="no-js">
     <head>
         <!-- META DATA -->
         <meta charset="<?php bloginfo( 'charset' ); ?>">
@@ -28,33 +28,51 @@
                             <img src="<?php bloginfo('template_directory'); ?>/_assets/media/images/logo.jpg" alt="<?php bloginfo('name'); ?>" />
                         </a>
                     </div>
-                    <div class="masthead-nav" role="navigation">
-                        <ul class="nav">
-                            <?php
-                                $nav_items = wp_get_nav_menu_items(4);
-                                foreach ((array)$nav_items as $key => $nav_item ) {
-                                    $title = $nav_item->title;
-                                    $url = $nav_item->url;
-                                    echo '<li class="nav-item"><a href="' . $url . '">' . $title . '</a></li>' . "\n";
-                                }
-                            ?>
-                            <li class="nav-item">
-                                <ul class="nav-item-subnav">
-                                    <?php if(get_field('facebook_link', 'option')): ?><li class="nav-item-subnav-item"><a href="<?php the_field('facebook_link', 'option'); ?>">
+                    <div class="masthead-nav">
+                        <div class="nav" role="navigation">
+                            <ul class="nav-list">
+                                <?php
+                                    $nav_items = wp_get_nav_menu_items(4);
+                                    foreach ((array)$nav_items as $key => $nav_item ) {
+                                        $title = $nav_item->title;
+                                        $url = $nav_item->url;
+                                ?>
+                                <li><a href="<?php echo $url ?>"><?php echo $title ?></a></li>
+                                <?php } ?>
+                            </ul>
+<!--
+                            <ul class="nav-social">
+                                <?php if(get_field('facebook_link', 'option')): ?>
+                                <li class="nav-list-item-subnav-item">
+                                    <a href="<?php the_field('facebook_link', 'option'); ?>">
                                         <img src="<?php bloginfo('template_directory'); ?>/_assets/media/images/icn-facebook.svg" alt="Visit Melissa Oholendt on Facebook" />
-                                    </a></li><?php endif; ?>
-                                    <?php if(get_field('twitter_link', 'option')): ?><li class="nav-item-subnav-item"><a href="<?php the_field('twitter_link', 'option'); ?>">
+                                    </a>
+                                </li>
+                                <?php endif; ?>
+                                <?php if(get_field('twitter_link', 'option')): ?>
+                                <li class="nav-list-item-subnav-item">
+                                    <a href="<?php the_field('twitter_link', 'option'); ?>">
                                         <img src="<?php bloginfo('template_directory'); ?>/_assets/media/images/icn-twitter.svg" alt="Visit Melissa Oholendt on Twitter" />
-                                    </a></li><?php endif; ?>
-                                    <?php if(get_field('instagram_link', 'option')): ?><li class="nav-item-subnav-item"><a href="<?php the_field('instagram_link', 'option'); ?>">
+                                    </a>
+                                </li>
+                                <?php endif; ?>
+                                <?php if(get_field('instagram_link', 'option')): ?>
+                                <li class="nav-list-item-subnav-item">
+                                    <a href="<?php the_field('instagram_link', 'option'); ?>">
                                         <img src="<?php bloginfo('template_directory'); ?>/_assets/media/images/icn-instagram.svg" alt="Visit Melissa Oholendt on Instagram" />
-                                    </a></li><?php endif; ?>
-                                    <?php if(get_field('pinterest_link', 'option')): ?><li class="nav-item-subnav-item"><a href="<?php the_field('pinterest_link', 'option'); ?>">
+                                    </a>
+                                </li>
+                                <?php endif; ?>
+                                <?php if(get_field('pinterest_link', 'option')): ?>
+                                <li class="nav-list-item-subnav-item">
+                                    <a href="<?php the_field('pinterest_link', 'option'); ?>">
                                         <img src="<?php bloginfo('template_directory'); ?>/_assets/media/images/icn-pinterest.svg" alt="Visit Melissa Oholendt on Pinterest" />
-                                    </a></li><?php endif; ?>
-                                </ul>
-                            </li>
-                        </ul>
+                                    </a>
+                                </li>
+                                <?php endif; ?>
+                            </ul>
+-->
+                        </div>
                     </div>
                 </div>
             </div>
