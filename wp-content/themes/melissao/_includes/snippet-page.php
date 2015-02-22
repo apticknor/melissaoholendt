8d95421daf7c685c7d556bca9848e7544406689c
Anthony Ticknor <tony.ticknor@gmail.com>
2015-02-22 15:13:55 -0600

    adding section media to pages

--- wp-content/themes/melissao/_includes/snippet-page.php
+++ wp-content/themes/melissao/_includes/snippet-page.php
@@ -15,13 +15,17 @@
             <div class="section-hd">
                 <h1 class="isVisuallyHidden"><?php the_title(); ?></h1>
             </div>
+            <?php if (has_post_thumbnail()) { ?>
+            <div class="section-media">
+                <img src="<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>" alt="" />
+            </div>
+            <?php } ?>
             <div class="section-bd">
                 <div class="userContent">
                     <?php the_content(); ?>
                 </div>
             </div>
         </div>
-
     <?php endwhile; ?>
 
     </div>
