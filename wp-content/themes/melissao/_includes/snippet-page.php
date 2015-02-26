d1cd5838b915e6293a3ce5ea1782ec8d49d88f4f
Anthony Ticknor <tony.ticknor@gmail.com>
2015-02-25 23:00:49 -0600

    adding faq section and styles

--- wp-content/themes/melissao/_includes/snippet-page.php
+++ wp-content/themes/melissao/_includes/snippet-page.php
@@ -25,6 +25,22 @@
                     <?php the_content(); ?>
                 </div>
             </div>
+            <?php if( have_rows('faq') ): ?>
+            <div class="section-ancillary">
+                <?php while ( have_rows('faq') ) : the_row(); ?>
+                <div class="faq">
+                    <div class="faq-question">
+                        <?php the_sub_field('faq_question'); ?>
+                    </div>
+                    <div class="faq-answer">
+                        <div class="userContent">
+                            <?php the_sub_field('faq_answer'); ?>
+                        </div>
+                    </div>
+                </div>
+                <?php endwhile; ?>
+            </div>
+            <?php endif; ?>
         </div>
     <?php endwhile; ?>
 
