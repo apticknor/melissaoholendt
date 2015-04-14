b53c27fdf909d43fc51067f9571473a970c241db
Anthony Ticknor <tony.ticknor@gmail.com>
2015-04-13 19:49:39 -0500

    reworking masthead styles as well as fonts and colors to match new branding

--- wp-content/themes/melissao/footer.php
+++ wp-content/themes/melissao/footer.php
@@ -20,3 +20,37 @@
         <?php wp_footer(); ?>
     </body>
 </html>
+
+
+<!--
+<ul class="nav-social">
+    <?php if(get_field('facebook_link', 'option')): ?>
+    <li class="nav-list-item-subnav-item">
+        <a href="<?php the_field('facebook_link', 'option'); ?>">
+            <img src="<?php bloginfo('template_directory'); ?>/_assets/media/images/icn-facebook.svg" alt="Visit Melissa Oholendt on Facebook" />
+        </a>
+    </li>
+    <?php endif; ?>
+    <?php if(get_field('twitter_link', 'option')): ?>
+    <li class="nav-list-item-subnav-item">
+        <a href="<?php the_field('twitter_link', 'option'); ?>">
+            <img src="<?php bloginfo('template_directory'); ?>/_assets/media/images/icn-twitter.svg" alt="Visit Melissa Oholendt on Twitter" />
+        </a>
+    </li>
+    <?php endif; ?>
+    <?php if(get_field('instagram_link', 'option')): ?>
+    <li class="nav-list-item-subnav-item">
+        <a href="<?php the_field('instagram_link', 'option'); ?>">
+            <img src="<?php bloginfo('template_directory'); ?>/_assets/media/images/icn-instagram.svg" alt="Visit Melissa Oholendt on Instagram" />
+        </a>
+    </li>
+    <?php endif; ?>
+    <?php if(get_field('pinterest_link', 'option')): ?>
+    <li class="nav-list-item-subnav-item">
+        <a href="<?php the_field('pinterest_link', 'option'); ?>">
+            <img src="<?php bloginfo('template_directory'); ?>/_assets/media/images/icn-pinterest.svg" alt="Visit Melissa Oholendt on Pinterest" />
+        </a>
+    </li>
+    <?php endif; ?>
+</ul>
+-->
--- wp-content/themes/melissao/header.php
+++ wp-content/themes/melissao/header.php
@@ -20,7 +20,7 @@
     </head>
     <body <?php body_class(); ?>>
 
-        <div class="tier">
+        <div class="tier tier_brand">
             <div class="wrapper">
                 <div class="masthead" role="banner">
                     <div class="masthead-logo">
@@ -32,52 +32,26 @@
                             <?php endif; ?>
                         </a>
                     </div>
-                    <div class="masthead-nav">
-                        <div class="nav" role="navigation">
-                            <ul class="nav-list">
-                                <?php
-                                    $nav_items = wp_get_nav_menu_items(21);
-                                    foreach ((array)$nav_items as $key => $nav_item ) {
-                                        $title = $nav_item->title;
-                                        $url = $nav_item->url;
-                                ?>
-                                <li><a href="<?php echo $url ?>"><?php echo $title ?></a></li>
-                                <?php } ?>
-                            </ul>
-<!--
-                            <ul class="nav-social">
-                                <?php if(get_field('facebook_link', 'option')): ?>
-                                <li class="nav-list-item-subnav-item">
-                                    <a href="<?php the_field('facebook_link', 'option'); ?>">
-                                        <img src="<?php bloginfo('template_directory'); ?>/_assets/media/images/icn-facebook.svg" alt="Visit Melissa Oholendt on Facebook" />
-                                    </a>
-                                </li>
-                                <?php endif; ?>
-                                <?php if(get_field('twitter_link', 'option')): ?>
-                                <li class="nav-list-item-subnav-item">
-                                    <a href="<?php the_field('twitter_link', 'option'); ?>">
-                                        <img src="<?php bloginfo('template_directory'); ?>/_assets/media/images/icn-twitter.svg" alt="Visit Melissa Oholendt on Twitter" />
-                                    </a>
-                                </li>
-                                <?php endif; ?>
-                                <?php if(get_field('instagram_link', 'option')): ?>
-                                <li class="nav-list-item-subnav-item">
-                                    <a href="<?php the_field('instagram_link', 'option'); ?>">
-                                        <img src="<?php bloginfo('template_directory'); ?>/_assets/media/images/icn-instagram.svg" alt="Visit Melissa Oholendt on Instagram" />
-                                    </a>
-                                </li>
-                                <?php endif; ?>
-                                <?php if(get_field('pinterest_link', 'option')): ?>
-                                <li class="nav-list-item-subnav-item">
-                                    <a href="<?php the_field('pinterest_link', 'option'); ?>">
-                                        <img src="<?php bloginfo('template_directory'); ?>/_assets/media/images/icn-pinterest.svg" alt="Visit Melissa Oholendt on Pinterest" />
-                                    </a>
-                                </li>
-                                <?php endif; ?>
-                            </ul>
--->
-                        </div>
+                    <div class="masthead-tagline">
+                        Weddings & Lifestyle
                     </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="tier">
+            <div class="wrapper">
+                <div class="nav" role="navigation">
+                    <ul class="nav-list">
+                        <?php
+                            $nav_items = wp_get_nav_menu_items(21);
+                            foreach ((array)$nav_items as $key => $nav_item ) {
+                                $title = $nav_item->title;
+                                $url = $nav_item->url;
+                        ?>
+                        <li><a href="<?php echo $url ?>"><?php echo $title ?></a></li>
+                        <?php } ?>
+                    </ul>
                 </div>
             </div>
         </div>
