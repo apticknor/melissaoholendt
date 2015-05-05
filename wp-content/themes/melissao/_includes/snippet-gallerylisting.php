62df00f707fde0f74aab5df4fab86bef5a5ccb2d
Anthony Ticknor <tony.ticknor@gmail.com>
2015-05-04 23:02:51 -0500

    CSS updates

--- wp-content/themes/melissao/_includes/snippet-gallerylisting.php
+++ wp-content/themes/melissao/_includes/snippet-gallerylisting.php
@@ -5,14 +5,66 @@
  * @package melissao
  * @author Anthony Ticknor <tony.ticknor@gmail.com>
  */
+
+ $images = get_field('gallery_images');
+
 ?>
 
 <div class="tier">
     <div class="wrapper">
         <div class="content" role="main">
-            Gallery Archive Goes Here!
+            <div class="listing">
+                <div class="listing-hd">
+                    <h1 class="isVisuallyHidden">Galleries</h1>
+                </div>
+                <div class="listing-bd">
+                    <?php if (have_posts()): ?>
+                        <ul class="gallery">
+                        <?php rewind_posts(); while (have_posts()) : the_post(); ?>
+                            <li>
+                                <div class="tile">
+                                    <div class="tile-media">
+                                        <a href="<?php esc_url(the_permalink()); ?>">
+                                            <?php
+                                                $image_id = get_post_thumbnail_id();
+                                                $image_url = wp_get_attachment_image_src($image_id, 'medium', true);
+                                            ?>
+                                            <img src="<?php echo $image_url[0]; ?>" alt="" />
+                                        </a>
+                                    </div>
+                                    <div class="tile-hd">
+                                        <h2 class="hdg hdg_sm mix-hdg_serif mix-hdg_kerningNarrow">
+                                            <a href="<?php esc_url(the_permalink()); ?>">
+                                                <?php the_title(); ?>
+                                            </a>
+                                        </h2>
+                                    </div>
+                                    <div class="tile-ft">
+                                        <span class="hdg hdg_xxs">
+                                            <?php echo get_field("gallery_type", false);?>
+                                        </span>
+                                    </div>
+                                </div>
+                            </li>
+                        <?php endwhile; ?>
+                        </ul>
+                    <?php endif; ?>
+                </div>
+            </div>
         </div>
     </div>
 </div>
 
-
+<!--
+<?php if ($images) : ?>
+    <ul class="tiles">
+    <?php foreach ($images as $image) : ?>
+        <li>
+            <a href="#">
+                <img src="<?php echo $image['sizes']['medium']; ?>" alt="<?php echo $image['alt']; ?>" />
+            </a>
+        </li>
+    <?php endforeach; ?>
+    </ul>
+<?php endif;?>
+-->
--- wp-content/themes/melissao/_includes/snippet-search-results.php
+++ wp-content/themes/melissao/_includes/snippet-search-results.php
@@ -11,25 +11,25 @@
     <div class="wrapper">
         <div class="content" role="main">
             <div class="listing">
-                <div class="listing-hd">
-                    <h1 class="isVisuallyHidden">
+                <div class="listing-hd listing-hd_isVisible">
+                    <h1 class="hdg hdg_lg">
                         <?php if (is_search()) { echo "Search Results"; } ?>
                     </h1>
                 </div>
                 <?php if (have_posts()): ?>
                     <div class="listing-bd">
-                        <ol>
+                        <ol class="vlist">
                         <?php rewind_posts(); while (have_posts()) : the_post(); ?>
                             <li>
-                                <div>
-                                    <div>
+                                <div class="post">
+                                    <div class="post-meta">
                                         <h2 class="hdg hdg_md">
                                             <a href="<?php esc_url(the_permalink()); ?>">
                                                 <?php the_title(); ?>
                                             </a>
                                         </h2>
                                     </div>
-                                    <div>
+                                    <div class="post-bd">
                                         <?php the_excerpt(); ?>
                                     </div>
                                 </div>
